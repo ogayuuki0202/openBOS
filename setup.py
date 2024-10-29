bc7c15ab206466fead043d5da681789826fd49ca
DESKTOP_YUKI_wsl <ogayuuki0202@gmail.com>
2024-10-30 00:14:55 +0900

    build

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ PACKAGES = [
 
 CLASSIFIERS = [
     'Intended Audience :: Science/Research',
-    'License :: Apache LicenseVersion 2.0',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',

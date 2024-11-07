e12ecdfb45b9ebc50c44bbfbaeb2943036fe3a31
DESKTOP_YUKI_wsl <ogayuuki0202@gmail.com>
2024-11-07 16:24:57 +0900

    pages

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -30,5 +30,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 html_static_path = ['_static']
